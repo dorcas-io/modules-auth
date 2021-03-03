6dabde7ebbf01558ef11b858d2eab731331e0687
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2021-03-03 17:02:09 +0100

    route fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 Route::group(['namespace' => 'Dorcas\ModulesAuth\Http\Controllers', 'prefix' => 'auth'], function() {
-	Route::get('/', 'ModulesAuthController@business')->name('dashboard');
-	Route::get('/setup', 'ModulesAuthController@welcome_setup')->name('welcome-setup');
-	Route::post('/setup', 'ModulesAuthController@welcome_post');
-	Route::get('/overview', 'ModulesAuthController@welcome_overview')->name('welcome-overview');
-	Route::post('/resend-verification', 'ModulesAuthController@resendVerification');
+
 });
 
 ?>
