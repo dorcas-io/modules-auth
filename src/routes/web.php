09852a6036c3405a801b528dc61f75264f960d7f
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2021-03-03 16:57:50 +0100

    route fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
-// Route::group(['namespace' => 'Dorcas\ModulesAuth\Http\Controllers', 'prefix' => 'auth'], function() {
-// 	/*Route::get('/', 'ModulesAuthController@business')->name('dashboard');
-// 	Route::get('/setup', 'ModulesAuthController@welcome_setup')->name('welcome-setup');
-// 	Route::post('/setup', 'ModulesAuthController@welcome_post');
-// 	Route::get('/overview', 'ModulesAuthController@welcome_overview')->name('welcome-overview');
-// 	Route::post('/resend-verification', 'ModulesAuthController@resendVerification');*/
-
-// 	/*Auth::routes();
-// 	Route::get('/logout', 'Auth\LoginController@logout');
-// 	Route::get('/forgot-password', 'Auth\ForgotPasswordController@showLinkRequestForm');
-// 	Route::post('/forgot-password', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('forgot-password');
-// 	Route::get('/reset-password/{token}', 'Auth\ResetPasswordController@showResetForm');
-// 	Route::post('/reset-password/{token}', 'Auth\ResetPasswordController@reset')->name('forgot-password-reset');
-// 	Route::get('/verify-email/{id}', 'Auth\Verify@verifyEmail')->name('verify-email');*/
-
-
-
-// });
+Route::group(['namespace' => 'Dorcas\ModulesAuth\Http\Controllers', 'prefix' => 'auth'], function() {
+	Route::get('/', 'ModulesAuthController@business')->name('dashboard');
+	Route::get('/setup', 'ModulesAuthController@welcome_setup')->name('welcome-setup');
+	Route::post('/setup', 'ModulesAuthController@welcome_post');
+	Route::get('/overview', 'ModulesAuthController@welcome_overview')->name('welcome-overview');
+	Route::post('/resend-verification', 'ModulesAuthController@resendVerification');
+});
 
 ?>
