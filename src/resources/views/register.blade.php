70b22bce2d3f3394c23533f7cdc633c2def7393a
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2022-06-24 10:53:45 +0100

    debug fix

--- src/resources/views/register.blade.php
+++ src/resources/views/register.blade.php
@@ -93,7 +93,7 @@
                         </div>
 
                         @if ( (env("DORCAS_EDITION", "business") == "community" || env("DORCAS_EDITION", "business") == "enterprise") && $userHostMode == "partner" )
-                            <div class="row">{{ $userHostMode }}
+                            <div class="row">
                                 <label class="form-label" for="domain">Choose and reserve a unique prefix (short name) for your business <em>(e.g. abc for ABC Limited)</em></label>
                                 <div class="input-group mb-2">
                                     <input type="text" class="form-control" required v-on:keyup="removeStatus()" placeholder="Entered desired prefix..." id="domain" name="domain" maxlength="80" v-model="domain">
